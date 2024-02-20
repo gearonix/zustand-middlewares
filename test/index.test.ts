e3d4c0a93c8b9974d7d17cb6fe83b29c91d21941
Uzhanin Egor <egor.uzhanin@gmail.com>
2024-02-20 07:06:15 +0300

    fix: tests

--- test/index.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe } from 'vitest'
-import { expect }   from 'vitest'
-import { it }       from 'vitest'
-
-describe('should', () => {
-  it('exported', () => {
-    expect(1).toEqual(1)
-  })
-})
